b544e9e8952045586675f34861ffbddf4d88dd5a
Anton Maminov <anton.linux@gmail.com>
2024-01-08 18:36:47 +0200

    Update parse.cr

--- samples/parse.cr
+++ samples/parse.cr
@@ -1,17 +1,13 @@
 require "../src/bplist"
 
-Bplist::Parser.debug = true
+# Bplist::Parser.debug = true
 
 # diff -y <(xxd assets/example.plist) <(xxd assets/example_mod.plist)
 
 # bplist = Bplist::Parser.new("#{__DIR__}/../assets/example.plist")
 bplist = Bplist::Parser.new("#{__DIR__}/../assets/example_mod.plist")
 
-begin
-  bplist.print_objects
-  result = bplist.parse
-  pp result
-  # pp result.as_h.keys
-ensure
-  # bplist.close
-end
+# bplist.print_objects
+result = bplist.parse
+pp result
+# pp result.as_h.keys
