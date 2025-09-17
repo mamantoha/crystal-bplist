998ce348b18b64bc3390bda416f91e09249b2c2c
Anton Maminov <anton.linux@gmail.com>
2025-09-17 18:14:05 +0300

    v0.2.0

--- src/bplist.cr
+++ src/bplist.cr
@@ -2,7 +2,7 @@ require "bytes_ext"
 require "./bplist/*"
 
 module Bplist
-  VERSION = "0.1.0"
+  VERSION = {{ `shards version #{__DIR__}`.chomp.stringify }}
 
   # Generic Bplist error.
   class Error < Exception
